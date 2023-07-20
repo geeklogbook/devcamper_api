2cf25eb7ec100166f69a91f215afd0b4535b5143
jino.michel.aque <contact@geeklogbook.com>
2023-07-20 00:38:23 -0300

    add mongo connection

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+//Load env vars
+dotenv.config({path: './config/config.env'});
+
+connectDB();
 
 //Routes files
 const bootcamps = require('./routes/bootcamps')
 
-dotenv.config({path: './config/config.env'});
 
 const app = express();
 
@@ -20,4 +26,10 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000 ;
 
 app.listen(PORT, 
-           console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
+           console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold));
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`${err.message}`.red);
+    //Close server & exit process
+    ServiceWorkerRegistration.close(() => process.exit(1));
+})
