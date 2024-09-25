f70401b7d579afd8e140886bbdc9830b84e506dd
geeklogbook <contact@geeklogbook.com>
2024-09-25 09:52:37 -0300

    Add Mongoose

--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,30 @@
-const errorHandler = (err, req, res, next) => {
-    console.log(err.stack.red);
+const ErrorResponse = require('../utils/errorResponse')
 
-    res.status(err.statusCode || 500).json({
+const errorHandler = (err, req, res, next) => {
+    let error = { ...err }
+
+    error.message = err.message;
+
+    console.log(err);
+
+    if(err.name==='CastError'){
+        const message = `Resource not found with id of ${err.value}`;
+        error = new ErrorResponse(message, 404)
+    }
+
+    if(err.code === 11000){
+        const message = 'Duplicate field value';
+        error = new ErrorResponse(message, 400);
+    }
+
+    if(err.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
+    }
+
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message || 'Server Error'
+        error: error.message || 'Server Error'
     });
 };
 
