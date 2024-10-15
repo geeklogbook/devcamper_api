55fe187f5f4a215a3f0c4b85804df2f3fcaed27e
geeklogbook <contact@geeklogbook.com>
2024-10-15 08:42:12 -0300

    Authentication and Permissions

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -29,26 +29,40 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route   POST /api/v1/bootcamps
 // @access  Private  
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
-        const bootcamp = await Bootcamp.create(req.body);
+    req.body.user = req.user.id;
 
-        res.status(201).json({
-            success: true, 
-            data: bootcamp
-        });
+    const publishedBootcamp = await Bootcamp.findOne( {user: req.user.id });
+
+    if(publishedBootcamp && req.user.role != 'admin'){
+        return next(new ErrorResponse(`The user with ID ${req.user.id} has already published a bootcamp`, 404))
+    }
+
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+        success: true, 
+        data: bootcamp
+    });
 })
 
 // @desc    Update new bootcamp
 // @route   PUT /api/v1/bootcamps/:id
 // @access  Private  
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-            new : true,
-            runValidators: true
-        });
+        let bootcamp = await Bootcamp.findById(req.params.id); 
     
         if (!bootcamp) {
             return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404));
         }
+
+        if(bootcamp.user.toString() != req.user.id && req.user.role !== 'admin'){
+            return next(new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`,401));
+        }
+
+        bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
     
         res.status(200).json({success: true, data: bootcamp });
 })
@@ -63,6 +77,10 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
         return next(
             new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404)
         );
+    }
+
+    if(bootcamp.user.toString() != req.user.id && req.user.role !== 'admin'){
+        return next(new ErrorResponse(`User ${req.params.id} is not authorized to delete this bootcamp`,401));
     }
 
     await bootcamp.deleteOne();
@@ -104,6 +122,10 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
         return next(
             new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404)
         );
+    }
+
+    if(bootcamp.user.toString() != req.user.id && req.user.role !== 'admin'){
+        return next(new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`,401));
     }
 
     if (!req.files) {
--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({path: './config/config.env'});
 
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -19,10 +20,15 @@ const courses = JSON.parse(
     fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8')
 );
 
+const users = JSON.parse(
+    fs.readFileSync(`${__dirname}/_data/users.json`,'utf-8')
+);
+
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
 
         console.log('Data Imported...'.green.inverse);
         process.exit();
@@ -35,6 +41,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
 
         console.log('Data Destroyed...'.red.inverse);
         process.exit();
