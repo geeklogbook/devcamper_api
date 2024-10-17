bdae387c1a41a061124f993c54928e48d62690f9
geeklogbook <contact@geeklogbook.com>
2024-10-17 00:52:17 -0300

    send mail for recover password

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken')
@@ -36,8 +37,12 @@ const UserSchema = new mongoose.Schema({
 })
 
 UserSchema.pre('save', async function(next) {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+  if(!this.isModified('password')){
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 UserSchema.methods.getSignedJwtToken = function() {
@@ -48,6 +53,19 @@ UserSchema.methods.getSignedJwtToken = function() {
 
 UserSchema.methods.matchPassword = async function(enteredPassword){
   return await bcrypt.compare(enteredPassword, this.password);
+}
+
+UserSchema.methods.getResetPasswordToken = function() {
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.resetPasswordExpire = Date.now() + 10 * 60 *1000;
+
+  return resetToken;
 }
 
 module.exports = mongoose.model('User', UserSchema)
